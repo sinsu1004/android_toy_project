399ce7ecca649b81c385284247f074b9e918840f
sinsu <sinsu19012@gmail.com>
2021-12-02 22:40:12 +0900

    5 인물 퀴즈 완료

--- app/src/main/java/com/example/project/activity/humanquiz.java
+++ app/src/main/java/com/example/project/activity/humanquiz.java
@@ -12,27 +12,38 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.project.HttpConnection;
 import com.example.project.R;
+import com.example.project.jsontype.datatype;
 import com.example.project.settings;
+
+import org.json.JSONArray;
+import org.json.JSONException;
 
 import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class humanquiz extends AppCompatActivity {
 
     private TextView textView20;
     private ImageView imageView5;
+    private TextView textView22;
+    private ArrayList<String> datalist=new ArrayList<>();
     private Button next2;
     private Button pass2;
     private CountDownTimer countDownTimer;
     private static int MILLISINFUTURE;
     private static int count;
     private static int truenum = 0;
+    private int passnum;
     private static int COUNT_DOWN_INTERVAL = 1000;
 
 
@@ -41,27 +52,48 @@ public class humanquiz extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.humanquiz);
 
+        datatype a=new datatype();
+        a.setName("dd");
+        NetworkTask networkTask = new NetworkTask("http://192.168.0.14:5000/sinsu/humanquiz",a);
+        networkTask.execute();
         textView20=findViewById(R.id.textView20);
+        textView22=findViewById(R.id.textView22);
         imageView5=findViewById(R.id.imageView5);
         next2=findViewById(R.id.next2);
         pass2=findViewById(R.id.pass2);
+
         loadImageTask imageTask = new loadImageTask("https://pds.joongang.co.kr/news/component/htmlphoto_mmdata/201608/04/htm_2016080484837486184.jpg");
         imageTask.execute();
+        passnum=((settings) getApplication()).getPasscount();
         count=((settings) getApplication()).getCount();
         MILLISINFUTURE = 10 * 100 * count;
         countDownTimer();
         countDownTimer.start();
+        textView22.setText("정답개수: " + truenum + "  남은 PASS 횟수: " + String.valueOf(passnum));
         next2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                loadImageTask imageTask = new loadImageTask(datalist.get(0));
+                imageTask.execute();
+                datalist.remove(0);
+                truenum++;
+                textView22.setText("정답개수: " + truenum + "  남은 PASS 횟수: " + String.valueOf(passnum));
             }
         });
 
         pass2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                if(passnum!=0){
+                loadImageTask imageTask = new loadImageTask(datalist.get(0));
+                imageTask.execute();
+                datalist.remove(0);
+                passnum--;
+                textView22.setText("정답개수: " + truenum + "  남은 PASS 횟수: " + String.valueOf(passnum));
+                }
+                else {
+                    Toast.makeText(getApplicationContext(), "남은 paas 가 없습니다.", Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
@@ -109,6 +141,43 @@ public class humanquiz extends AppCompatActivity {
         }
     }
 
+    public class NetworkTask extends AsyncTask<Void, Void, String> {
+        private String url;
+        private datatype values;
+
+        public NetworkTask(String url, datatype values) {
+            this.url = url;
+            this.values = values;
+        }
+
+
+        @Override
+        protected String doInBackground(Void... voids) {
+            JSONArray jsonArray =new JSONArray();
+            //String result;  //요청 결괄르 저장할 변수
+            HttpConnection recon=new HttpConnection();
+            jsonArray=recon.request(url,values); //해당 URL로 부터 결과물을 얻어온다.
+            for(int i=0;i<jsonArray.length();i++){
+                try {
+                    datalist.add(jsonArray.getJSONObject(i).getString("image"));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {   //doInbackground()로 부터 리턴되 값이 onPostExecute()의 매개변수로 넘어오므로 s를 출력한다.
+            super.onPostExecute(s);
+
+            Collections.shuffle(datalist);
+
+        }
+    }
+
+
     public void countDownTimer() {     //카운트 다운
 
 
@@ -124,10 +193,10 @@ public class humanquiz extends AppCompatActivity {
                 Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
                 vibrator.vibrate(1000); // 0.5초간 진동
 
-//                Intent intent=new Intent(getApplicationContext(),test3Activity.class);
-//                intent.putExtra("truecount",truenum);
-//                startActivity(intent);
-//                finish();
+                Intent intent=new Intent(getApplicationContext(),test3Activity.class);
+                intent.putExtra("truecount",truenum);
+                startActivity(intent);
+                finish();
 
 
 
@@ -136,6 +205,17 @@ public class humanquiz extends AppCompatActivity {
 
         };
 
+
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        try{
+            countDownTimer.cancel();
+
+        }catch(Exception e) {}
+        countDownTimer=null;
 
     }
 }
